87778563489ba16c9822963c68fbe63e1260f0ff
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-08 02:43:57 +0200

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,5 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """obfuscated message generator"""
-    new_fields: List[str] = [f"(?<={x}=)[^;]*" for x in fields]
+    new_fields: List[str] = [f"(?<={x}=)[^{separator}]*" for x in fields]
     return re.sub("|".join(new_fields), redaction, message)
