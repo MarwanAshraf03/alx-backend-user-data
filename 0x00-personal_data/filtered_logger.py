137851dca975442dd7c60504a3866b7030aef3d9
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-08 03:20:38 +0200

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """a filtered logger module"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -9,3 +10,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """obfuscated message generator"""
     new_fields: List[str] = [f"(?<={x}=)[^{separator}]*" for x in fields]
     return re.sub("|".join(new_fields), redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.__fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formatting the record"""
+        record.msg = filter_datum(self.__fields, self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
