0b306e23b42bfbb5e252cb3a486aa180cae5dc82
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-09 20:52:58 +0200

    6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """a function that hashes a password"""
     return bcrypt.hashpw(bytes(password, "UTF-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """a function to validate a password"""
+    return bcrypt.hashpw(bytes(password, "UTF-8"), hashed_password)\
+        == hashed_password
