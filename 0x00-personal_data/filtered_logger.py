6a7f28e46487243b5f8fd95880c8532e8a922fa6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-10 00:01:37 +0200

    4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -73,10 +73,10 @@ def main():
     cursor.execute("SELECT * FROM users;")
     for row in cursor:
         # ';'.join(row)
-        print(RedactingFormatter(list(PII_FIELDS)).format(logger.makeRecord(logger.name, logger.level,None, None,";".join([str(x) for x in row]), None, None)))
+        # print(RedactingFormatter(list(PII_FIELDS)).format(logger.makeRecord(logger.name, logger.level,None, None,";".join([str(x) for x in row]), None, None)))
         # print(";".join([str(x) for x in row]))
         # print(type(row))
-        # print(row)
+        print(row)
     cursor.close()
     db.close()
 
