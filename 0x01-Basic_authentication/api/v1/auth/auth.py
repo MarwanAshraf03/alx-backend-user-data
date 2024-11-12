25c01f64f61eb68bb1d3ed55f3fd01a4337b8b03
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-12 17:55:07 +0200

    3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+"""template module for all authentication system"""
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns flase"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """returns the authorization header"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """returns the current user"""
         return None
