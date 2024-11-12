6731aa3255b2f0274e71fe8293fb030e0174b827
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-12 22:06:18 +0200

    7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """interits from Auth"""
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """extracts the header without the `Basic ` word`"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header).__name__ != "str":
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.replace("Basic ", "")
