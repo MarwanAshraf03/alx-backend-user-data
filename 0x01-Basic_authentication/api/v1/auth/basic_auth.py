e373af1ebbe7ce858334a7a3caf8446d0517aaa9
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-12 23:37:48 +0200

    9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """template module for all authentication system"""
 from base64 import decode, b64decode, b64encode
+from typing import Tuple
 from api.v1.auth.auth import Auth
 
 
@@ -30,3 +31,15 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return bytes.decode(b64decode(base64_authorization_header), "utf-8")
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """returns the user's name and password"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header).__name__ != "str":
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        creds = decoded_base64_authorization_header.split(":")
+        return (creds[0], creds[1])
