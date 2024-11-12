4b3f75c599ee00ca80d23ef9b69ff11dccbc50f4
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-12 21:27:12 +0200

    6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,7 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth, BasicAuth
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -60,5 +61,5 @@ if __name__ == "__main__":
     if getenv("AUTH_TYPE") == "auth":
         auth = Auth()
     if getenv("AUTH_TYPE") == "basic_auth":
-        auth = BasicAuth ()
+        auth = BasicAuth()
     app.run(host=host, port=port, debug=True)
