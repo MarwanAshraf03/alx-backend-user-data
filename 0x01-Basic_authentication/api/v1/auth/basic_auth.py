7bac5675f2f2e47b78360d342fc8e61003f066aa
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-13 00:45:59 +0200

    10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,11 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or type(user_pwd).__name__ != "str":
             return None
-        user_list = User.search({"email": user_email})
+        try:
+            user_list = User.search({"email": user_email})
+        except KeyError:
+            return None
+        print(user_list)
         user = None
         for i in user_list:
             if user_email in i.__dict__.values():
