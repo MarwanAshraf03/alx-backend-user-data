52603907c635d8a019c577f27ee9ff8a54e068fe
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-15 14:51:42 +0200

    2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 """A session authenticator module that inherits from AUTH class"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Authentication class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session"""
+        if user_id is None:
+            return None
+        if type(user_id).__name__ != (str):
+            return None
+        idd = uuid4()
+        self.user_id_by_session_id[idd] = user_id
+        return idd
