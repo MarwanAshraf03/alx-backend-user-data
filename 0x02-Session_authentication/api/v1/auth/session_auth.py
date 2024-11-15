a40ad3eeb0d11d154cb8099da711ebaa2c74cbd8
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-15 14:45:28 +0200

    1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from api.v1.auth.auth import Auth
 """A session authenticator module that inherits from AUTH class"""
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
