6af2d9b2164e7da4cd87a8342b717d2763f1b456
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-16 16:50:21 +0200

    9

--- 0x02-Session_authentication/mock.py
+++ 0x02-Session_authentication/mock.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
-
+from datetime import datetime
+from time import sleep
+first = datetime.now()
+sleep(3)
+second = datetime.now()
+print(first)
+print(second)
+print(second-first)
+print(type(second-first))
+print((second-first).seconds)
