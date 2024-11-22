06ce2ccbff3262606c71a56a45b60e6d6d4ae4a2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-23 00:48:18 +0200

    19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,6 +68,7 @@ def get_reset_password_token():
 
 @app.route("/reset_password", methods=['PUT'], strict_slashes=False)
 def update_password():
+    """updates a password in method put"""
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
