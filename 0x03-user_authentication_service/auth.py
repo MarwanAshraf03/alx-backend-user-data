fdcb861c944240496e4797ae2039ec00f4803530
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-22 23:39:41 +0200

    5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,24 +18,23 @@ def _hash_password(password: str) -> bytes:
 #     return str(uuid4())
 
 
-# class Auth:
-#     """Auth class to interact with the authentication database.
-#     """
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
-#     def __init__(self):
-#         self._db = DB()
+    def __init__(self):
+        self._db = DB()
 
-#     def register_user(self, email: str, password: str) -> User:
-#         """registers user to database"""
-#         user = None
-#         try:
-#             self._db.find_user_by(email=email)
-#         except NoResultFound:
-#             user = self._db.add_user(email, _hash_password(password))
-#         if user is None:
-#             raise ValueError(f"User {email} already exists")
-#         # print(f"user is {user}")
-#         return user
+    def register_user(self, email: str, password: str) -> User:
+        """registers user to database"""
+        user = None
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+        if user is None:
+            raise ValueError(f"User {email} already exists")
+        return user
 
 #     def valid_login(self, email: str, password: str) -> bool:
 #         """checks if user is valid or not"""
