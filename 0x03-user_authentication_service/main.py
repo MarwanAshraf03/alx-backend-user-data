151c6d92edcef3ca18bae2963ed0f07612e847e2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-22 23:46:59 +0200

    8-

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -8,7 +8,16 @@ email = 'bob@bob.com'
 password = 'MyPwdOfBob'
 auth = Auth()
 
+user = auth.register_user(email, password)
+user2 = auth.register_user("email", password)
+user3 = auth.register_user("email2", password)
+
 auth.register_user(email, password)
 
 print(auth.create_session(email))
+print(auth.create_session("email2"))
 print(auth.create_session("unknown@email.com"))
+
+print(auth.get_user_from_session_id(user.session_id))
+print(auth.get_user_from_session_id(user2.session_id))
+print(auth.get_user_from_session_id(user3.session_id))
